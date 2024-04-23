e84742396b49f2ab77977e817e11d089ee81defa
German Horianski <germanezequielhorianski@gmail.com>
2024-04-22 21:54:57 -0300

    Syle. Mimic moralis money searchbar

--- src/components/containers/SearchBar.tsx
+++ src/components/containers/SearchBar.tsx
@@ -1,11 +1,27 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
+
 const SearchBar = () => {
   return (
-    <div className="bg-[#09182a] h-[12%] flex p-2">
-        <div className="w-[50%] flex justify-start">
-            <button className="bg-[#06111d] border border-solid border-[#224767] border-1 text-[#2e628e] text-xs leading-loose flex justify-start px-6 py-2 w-full rounded-full">Search for any token or wallet </button>
+    <header className="bg-[#09182a] h-[4.375rem] flex justify-between sticky p-3 border-b border-solid border-[#132a44]">
+    <div className="flex grow w-full max-w-[28.125rem]">
+        <div className="flex grow w-full">
+            <button className="bg-[#06111d] border border-solid border-[#224767] border-1 text-[#2e628e] text-[0.875rem] gap-[0.625rem] leading-loose flex justify-start items-center py-[0.5rem] px-[1.37rem] w-full rounded-full leading-[1.5]">
+              <FontAwesomeIcon icon={faMagnifyingGlass}/>
+              <span className="flex items-center pointer-events-none">Search for any token or wallet</span>
+            </button>
         </div>
-        <button>Login / Connect</button>
     </div>
+    <div className='flex items-center justify-end gap-3'>
+        <div className="h-[80%]">
+          <button className='flex justify-center items-center border border-[#2e628e] text-[#85b3db] font-medium text-[0.875rem] px-2 gap-2 h-full border-2 border-solid rounded-lg'>
+            <FontAwesomeIcon icon={faArrowRightToBracket} />
+            Login / Connect
+            </button>
+        </div>
+    </div>
+    </header>
   )
 }
 
